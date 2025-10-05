f3b744d65141cc33ace9f8ca6d968248abc8be91
OnionRing <antoniocg285@gmail.com>
2025-10-05 22:53:32 +0200

    working on the notepad component
    added filename as an input and positioning to the notepad component

--- src/app/presentation/notepad/notepad.ts
+++ src/app/presentation/notepad/notepad.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, HostListener, input } from '@angular/core';
 
 @Component({
   selector: 'app-notepad',
@@ -6,11 +6,22 @@ import { Component, input } from '@angular/core';
   templateUrl: './notepad.html',
   styleUrl: './notepad.css'
 })
+
+
+
 export class Notepad {
 
+  // Window properties
   width = input.required<number>();
   height = input.required<number>();
   content = input.required<string>();
+  filename = input.required<string>();
+
+  // Positioning
+  top = input<number>(0);
+  left = input<number>(0);
+  bottom = input<number>(0);
+  right = input<number>(0);
 
 
 }
