7dfd873241ee02534cfeeb58a969de90c10eb183
OnionRing <antoniocg285@gmail.com>
2025-11-09 01:50:32 +0100

    Publishing changes on Github Pages at 11/09/2025 01:50:32

--- BuildAndPrepareAngularProject.ps1
+++ BuildAndPrepareAngularProject.ps1
@@ -16,7 +16,8 @@ if ($publishOnDist -eq $true -Or -Not $publishOnDist)
     #When we build on /dist, a folder with the project name is created.  
     $buildPath = ".\dist\"+$projectFolderName+"\browser" 
     $buildPathDestination =".\dist\"+$projectFolderName
-    ng build --configuration production --base-href "\"$projectFolderName"\"
+    $projectFolderName = "\"+$projectFolderName+"\"
+    ng build --configuration production --base-href $projectFolderName
 }else
 {
     Write-Output "Publishing on /docs folder"    
